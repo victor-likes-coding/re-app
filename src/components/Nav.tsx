0d4c837a8d2a378cadc38e07db13ed32408aa98e
Victor Tran <victor-likes-coding@tempestas.io>
2023-03-08 02:56:23 -0500

    import image and place into img tag

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import image from "../assets/no-background-logo.png";
 
 export default function Nav() {
     return (
+        <nav className="navbar">
+            <h3 className="navbar__logo-container">
+                <img
+                    className="navbar__logo"
+                    src={image}
+                    alt="re-app logo"
+                />
+            </h3>
             <div className="navbar__links">
                 <div className="navbar__auth primary-button">Sign In</div>
             </div>
