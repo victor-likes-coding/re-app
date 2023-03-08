40b8a31930d6ac50cd461c6db2326159256f6b73
Victor Tran <victor-likes-coding@tempestas.io>
2023-03-08 16:23:52 -0500

    add navigation to navbar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import image from "../assets/no-background-logo.png";
+import { useNavigate } from "react-router-dom";
 
 export default function Nav() {
+    const navigate = useNavigate();
     return (
         <nav className="navbar">
-            <h3 className="navbar__logo-container">
+            <h3
+                className="navbar__logo-container"
+                onClick={() => navigate("/", { replace: true })}>
                 <img
                     className="navbar__logo"
                     src={image}
@@ -12,7 +16,11 @@ export default function Nav() {
                 />
             </h3>
             <div className="navbar__links">
-                <div className="navbar__auth primary-button">Sign In</div>
+                <div
+                    onClick={() => navigate("/login", { replace: true })}
+                    className="navbar__auth primary-button">
+                    Sign In
+                </div>
             </div>
         </nav>
     );
