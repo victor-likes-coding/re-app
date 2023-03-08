3520bc24f3a8cec360fc0783fde1fa5ca94dc05b
Victor Tran <victor-likes-coding@tempestas.io>
2023-03-08 03:42:35 -0500

    add react router dom and use BrowserRouter

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./sass/index.scss";
+import { BrowserRouter as Router } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
     <React.StrictMode>
-        <App />
+        <Router>
+            <App />
+        </Router>
     </React.StrictMode>
 );
 
