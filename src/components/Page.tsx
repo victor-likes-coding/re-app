a57212bde41ace0abe20b4522dde1f3b5e766692
Victor Tran <victor-likes-coding@tempestas.io>
2023-03-08 03:32:10 -0500

    write page content

---
+++ src/components/Page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Page() {
+    return (
+        <div className="page">
+            <h1 className="page__title">Invest smarter with our user-centric suite of products.</h1>
+            <p className="page__para">Rentify is your one-stop-shop for real estate investment. List your property hassle-free and let renters find you.</p>
+            <div className="page__cta-container">
+                <div className="primary-button">Get Started</div>
+            </div>
+        </div>
+    );
+}
