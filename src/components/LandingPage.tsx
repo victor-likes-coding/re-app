720d51f1b5798c53cc34e7ef2bd1f6bfd5ab6ae8
Victor Tran <victor-likes-coding@tempestas.io>
2023-03-08 16:23:25 -0500

    update to split up sections

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -3,11 +3,21 @@ import React from "react";
 export default function LandingPage() {
     return (
         <div className="page">
-            <h1 className="page__title">Invest smarter with our user-centric suite of products.</h1>
-            <p className="page__para">Rentify is your one-stop-shop for real estate investment. List your property hassle-free and let renters find you.</p>
-            <div className="page__cta-container">
-                <div className="primary-button">Get Started</div>
-            </div>
+            <Section>
+                <h1 className="page__title">Invest smarter with our user-centric suite of products.</h1>
+                <p className="page__para">Rentify is your one-stop-shop for real estate investment. List your property hassle-free and let renters find you.</p>
+                <div className="page__cta-container">
+                    <div className="primary-button">Get Started</div>
+                </div>
+            </Section>
+
+            <Section>
+                <h2 className="page__title">Why Rentify?</h2>
+                <p className="page__para">
+                    Rentify is the best way to find a rental property. We have the largest selection of rental properties in the country, and we make it easy to
+                    find your next home.
+                </p>
+            </Section>
         </div>
     );
 }
