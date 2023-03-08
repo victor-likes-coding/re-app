363cb023778c9e8a9958296edc3015ab6d9b2423
Victor Tran <victor-likes-coding@tempestas.io>
2023-03-08 18:01:03 -0500

    create section coponent

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
-export default function Section({ children }: { children: React.ReactNode }) {
-    return <section className="section">{children}</section>;
+export default function Section({ children, className }: { children: React.ReactNode; className?: string }) {
+    return <section className={`section ${className ? className : ""}`}>{children}</section>;
 }
